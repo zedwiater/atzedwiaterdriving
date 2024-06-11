ab2a85c34a912476c05f3abb33f4a6f8bfda30dc
zedwiater <37824547+zedwiater@users.noreply.github.com>
2024-06-11 14:38:23 -0400

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,28 +1,40 @@
 let slideIndex = 0;
 showSlides();
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
-
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideIndex = n - 1);
 }
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
+
+setInterval(() => {
+    plusSlides(1);
+}, 3000); // Change image every 3 seconds
 
