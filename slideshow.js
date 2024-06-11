5c4527cc16ba17e4fa067f8ac186b070367cb90b
zedwiater <37824547+zedwiater@users.noreply.github.com>
2024-06-11 13:30:59 -0400

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,40 +1,28 @@
-let slideIndex = 1;
-let autoPlayTimer;
+let slideIndex = 0;
+showSlides();
 
-showSlide(slideIndex);
-autoPlaySlides();
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
 
 function plusSlides(n) {
-    clearInterval(autoPlayTimer);
-    showSlide(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
-function showSlide(n) {
-    let slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-function autoPlaySlides() {
-    autoPlayTimer = setInterval(function() {
-        showSlide(slideIndex += 1);
-    }, 6000); // Change 2000 to desired autoplay interval in milliseconds
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".prev").addEventListener("click", function() {
-        plusSlides(-1);
-    });
-
-    document.querySelector(".next").addEventListener("click", function() {
-        plusSlides(1);
-    });
-});
